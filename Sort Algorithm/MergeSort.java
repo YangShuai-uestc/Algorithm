505d18a0e554a4c618851397712ccaa8d37611b2
小率 <673258558@qq.com>
2020-07-20 21:15:20 +0800

    优化

--- Sort Algorithm/MergeSort.java
+++ Sort Algorithm/MergeSort.java
@@ -1,27 +1,37 @@
-import java.util.Arrays;
-
 public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        int[] arr = {49, 38, 65, 97, 76, 13, 27, 50};
-        mergeSort(arr, 0, arr.length - 1);
-        System.out.println(Arrays.toString(arr));
+        int max = 100000;
+        int[] test = new int[max];
+
+        for (int i = 0; i < max; i++) {
+            test[i] = (int) (Math.random() * max);
+        }
+
+        //排序开始时间
+        long start = System.currentTimeMillis();
+        int[] temp = new int[test.length];
+        mergeSort(test, temp, 0, test.length - 1);
+        //排序结束时间
+        long end = System.currentTimeMillis();
+
+
+        System.out.println(end - start);
 
     }
 
     /**
      * 有序数组的排序
      *
-     * @param a     前半段和后半段为两个有序数组
+     * @param a         前半段和后半段为两个有序数组
+     * @param arrayTemp 用来临时存放数据的数组
      * @param left
      * @param mid
      * @param right
      * @return 有序数组
      */
-    public static int[] merge(int[] a, int left, int mid, int right) {
-        //临时存放的数组
-        int[] tempArray = new int[a.length];
+    public static int[] merge(int[] a, int[] arrayTemp, int left, int mid, int right) {
         //左检测指针
         int p1 = left;
         //右检测指针
@@ -31,10 +41,10 @@ public class Main {
         while (p1 <= mid && p2 <= right) {
             //两个有序数组中取小的存进来
             if (a[p1] < a[p2]) {
-                tempArray[k] = a[p1];
+                arrayTemp[k] = a[p1];
                 p1++;
             } else {
-                tempArray[k] = a[p2];
+                arrayTemp[k] = a[p2];
                 p2++;
             }
             k++;
@@ -42,18 +52,18 @@ public class Main {
 
         //将未遍历完的部分直接存入数组
         while (p1 <= mid) {
-            tempArray[k] = a[p1];
+            arrayTemp[k] = a[p1];
             k++;
             p1++;
         }
 
         while (p2 <= right) {
-            tempArray[k] = a[p2];
+            arrayTemp[k] = a[p2];
             k++;
             p2++;
         }
 
-        System.arraycopy(tempArray, left, a, left, right - left + 1);
+        System.arraycopy(arrayTemp, left, a, left, right - left + 1);
 
         return a;
     }
@@ -61,19 +71,23 @@ public class Main {
     /**
      * 归并排序
      *
-     * @param a     待排序数组
-     * @param start 排序起点元素
-     * @param end   排序终点元素
+     * @param a         待排序数组
+     * @param arrayTemp 临时存放数据的空间
+     * @param start     排序起点元素
+     * @param end       排序终点元素
      */
-    public static void mergeSort(int[] a, int start, int end) {
+    public static void mergeSort(int[] a, int[] arrayTemp, int start, int end) {
         if (start < end) {
             //划分子数组
             int mid = (start + end) / 2;
-            //递归排序左数组
-            mergeSort(a, start, mid);
-            //递归排序右数组
-            mergeSort(a, mid + 1, end);
-            merge(a, start, mid, end);
+            //递归排序左数组，使之有序
+            mergeSort(a, arrayTemp, start, mid);
+            //递归排序右数组，使之有序
+            mergeSort(a, arrayTemp, mid + 1, end);
+            //临时存放数据的数组
+            int[] temp = new int[a.length];
+
+            merge(a, temp, start, mid, end);
         }
     }
 }
